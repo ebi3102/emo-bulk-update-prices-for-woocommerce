0dbdff1a71d1b7ba58259d7380fea8337c58056b
Ebrahim Moeini <ebi3102@gmail.com>
2023-01-22 22:55:01 +0330

    Create EMO_EWPU_NoticeTemplate class to render notice template

---
+++ includes/Classes/EMO_EWPU_NoticeTemplate.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package EWPU
+ * ========================
+ * Notice Template
+ * ========================
+ * Text Domain: emo_ewpu
+ */
+
+class EMO_EWPU_NoticeTemplate
+{
+
+    public function render_template($args)
+    {
+        
+    }
+}

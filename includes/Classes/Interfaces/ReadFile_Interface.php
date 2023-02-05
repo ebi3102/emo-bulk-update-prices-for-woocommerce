b3728eb83c71b41961d45dc410a0c723428687fa
Ebrahim Moeini <ebi3102@gmail.com>
2023-02-05 17:47:30 +0330

    Create ReadFile_Interface.php and WriteToFile_Interface.php interfaces

---
+++ includes/Classes/Interfaces/ReadFile_Interface.php
@@ -0,0 +1,12 @@
+<?php
+
+interface ReadFile_Interface {
+
+	/**
+	 * @param array $arg
+	 * @return array | false
+	 */
+	public function readFile(array $arg):array | false ;
+
+
+}
---
+++ includes/Classes/Interfaces/WriteToFile_Interface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Interface for implementing write on a file.
+ */
+interface WriteToFile_Interface {
+
+	/**
+	 * @param array $arg
+	 * @return void
+	 */
+	public function writeToFile(array $arg);
+
+}

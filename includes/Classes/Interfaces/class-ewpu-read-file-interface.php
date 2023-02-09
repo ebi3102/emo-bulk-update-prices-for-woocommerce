a6e297af78b0168a42dbcbeb3f2eb3e8c74d907c
Ebrahim Moeini <ebi3102@gmail.com>
2023-02-09 10:15:33 +0330

    Add documentation to file handler interfaces

--- includes/Classes/Interfaces/class-ewpu-read-file-interface.php
+++ includes/Classes/Interfaces/class-ewpu-read-file-interface.php
@@ -1,12 +1,13 @@
 <?php
-
+/**
+ * Defines a common functions that any class responsible for
+ * reading a file.
+ */
 interface EWPU_Read_File_Interface {
 
 	/**
-	 * @param array $arg
+	 * @param array $arg define any arguments that are needed to read a file
 	 * @return array | false
 	 */
 	public function readFile(array $arg):array | false ;
-
-
 }
--- includes/Classes/Interfaces/class-ewpu-write-file-interface.php
+++ includes/Classes/Interfaces/class-ewpu-write-file-interface.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Interface for implementing write on a file.
+ * Defines a common functions that any class responsible for
+ * writing on a file.
  */
 interface EWPU_Write_File_Interface {
 
 	/**
-	 * @param array $arg
+	 * @param array $arg define any arguments that are needed to write on a file
 	 * @return void
 	 */
 	public function writeToFile(array $arg);
-
 }

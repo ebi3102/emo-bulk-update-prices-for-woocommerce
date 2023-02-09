9ca179522e17b0fbdeffb4311b46aaebfd66cef9
Ebrahim Moeini <ebi3102@gmail.com>
2023-02-09 23:23:50 +0330

    Create two class of class-ewpu-form-error.php and class-ewpu-form-file-error.php to handle the errors of submitting forms

---
+++ includes/class-ewpu-form-error.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * A static class to inspect the various aspects of statuses of a form
+ * and handle the errors
+ *
+ * if any methods detect an error will return WP_Error class else false
+*/
+class EWPU_Form_Error {
+	private function __construct(){}
+
+	public static function submit_status(string $submitName)
+	{
+
+	}
+
+	public static function nonce_inspection(string $nonceName, string $nonceAction)
+	{
+
+	}
+
+	public static function requirement_inspection(string $fieldName){
+
+	}
+
+
+
+}

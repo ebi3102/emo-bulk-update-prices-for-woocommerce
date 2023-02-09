15fc76d74df55fc36c1a451a5365485aa080bf07
Ebrahim Moeini <ebi3102@gmail.com>
2023-02-09 13:07:25 +0330

    Edit a syntax error in form-functions.php

--- includes/functions/form-functions.php
+++ includes/functions/form-functions.php
@@ -58,7 +58,7 @@ function emo_ewpu_get_price_update_data(bool $is_submit): array
     $products = array();
     if($cat_id){
         $relatedProducts = $wpdb->get_results("SELECT object_id FROM $wpdb->term_relationships WHERE term_taxonomy_id = " . $cat_id);
-        if(is_array(relatedProducts) && count(relatedProducts) > 0) {
+        if(is_array($relatedProducts) && count($relatedProducts) > 0) {
             foreach ($relatedProducts as $relatedProduct) {
                 array_push($products, $relatedProduct->object_id);
             }

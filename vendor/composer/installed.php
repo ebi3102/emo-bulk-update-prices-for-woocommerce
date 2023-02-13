5644f3bc13ad439937fdd6b80f17054392b6e53f
Ebrahim Moeini <ebi3102@gmail.com>
2023-02-13 20:15:34 +0330

    install autoload by composer psr-4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'faaa8c89d83eb5634c9fad55ebd265a26451a1e8',
+        'name' => 'emo/emo-woo-price-update',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'emo/emo-woo-price-update' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'faaa8c89d83eb5634c9fad55ebd265a26451a1e8',
+            'dev_requirement' => false,
+        ),
+    ),
+);

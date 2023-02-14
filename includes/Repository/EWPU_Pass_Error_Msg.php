3edf198f89f344b1215dc925b769eaafb066128b
Ebrahim Moeini <ebi3102@gmail.com>
2023-02-14 14:51:52 +0330

    Create EWPU_Pass_Error_Msg.php class to pass error to WP_Error class

---
+++ includes/Repository/EWPU_Pass_Error_Msg.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Get error message and pass it to WordPress WP_Erroe
+*/
+namespace EmoWooPriceUpdate\Repository;
+
+class EWPU_Pass_Error_Msg {
+
+	private function __construct(){}
+
+	/**
+	 * Get error message and return as a WP_Error object
+	 * @param string $error
+	 * @param string $error_data
+	 *
+	 * @return WP_Error
+	 */
+	public static function error_object(string $error, string $error_data): WP_Error
+	{
+		return new WP_Error($error, $error_data);
+	}
+
+}

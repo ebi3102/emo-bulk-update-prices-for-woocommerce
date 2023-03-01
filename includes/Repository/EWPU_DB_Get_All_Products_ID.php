383da9251a9b86d197f3d7beda872038b0557845
Ebrahim Moeini <ebi3102@gmail.com>
2023-03-01 16:18:15 +0330

    Remove additional fields from query of EWPU_DB_Get_All_Products_ID.php

--- includes/Repository/EWPU_DB_Get_All_Products_ID.php
+++ includes/Repository/EWPU_DB_Get_All_Products_ID.php
@@ -7,6 +7,6 @@ class EWPU_DB_Get_All_Products_ID extends EWPU_DB
 {
 	public function __construct() {
 		parent::__construct();
-		$this->query = "SELECT ID, post_title FROM {$this->db_class->posts} WHERE post_type = 'product' AND post_status = 'publish' ORDER BY post_modified DESC;";
+		$this->query = "SELECT ID FROM {$this->db_class->posts} WHERE post_type = 'product' AND post_status = 'publish' ORDER BY post_modified DESC;";
 	}
 }

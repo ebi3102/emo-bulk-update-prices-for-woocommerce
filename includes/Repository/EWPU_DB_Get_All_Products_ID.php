d90255f15e8b2d7c4a8fb48037edfa452ffff9ba
Ebrahim Moeini <ebi3102@gmail.com>
2023-03-01 11:49:16 +0330

    Rename EWPU_DB_Get_All_Products class to EWPU_DB_Get_All_Products_ID and remove unused $term id parameter

--- includes/Repository/EWPU_DB_Get_All_Products_ID.php
+++ includes/Repository/EWPU_DB_Get_All_Products_ID.php
@@ -3,9 +3,9 @@
 namespace EmoWooPriceUpdate\Repository;
 use EmoWooPriceUpdate\Repository\EWPU_DB;
 
-class EWPU_DB_Get_All_Products extends EWPU_DB
+class EWPU_DB_Get_All_Products_ID extends EWPU_DB
 {
-	public function __construct($term_id) {
+	public function __construct() {
 		parent::__construct();
 		$this->query = "SELECT ID, post_title FROM {$this->db_class->posts} WHERE post_type = 'product' AND post_status = 'publish' ORDER BY post_modified DESC;";
 	}

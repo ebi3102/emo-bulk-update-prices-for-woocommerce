f9c2ae0b78d7d3aeabc94bba8ef950be45a888e2
Ebrahim Moeini <ebi3102@gmail.com>
2023-03-01 16:20:32 +0330

    Create EWPU_Product class to create an object for a product

--- includes/Repository/WOO_Product/EWPU_Product.php
+++ includes/Repository/WOO_Product/EWPU_Product.php
@@ -4,9 +4,42 @@ namespace EmoWooPriceUpdate\Repository\WOO_Product;
 class EWPU_Product {
 	private $product;
 
+
 	public function __construct($id) {
 		$this->product = wc_get_product($id);
 	}
 
+	public function get_product_type()
+	{
+		return $this->product->get_type();
+	}
+
+	public function get_product_sku()
+	{
+		return $this->product->get_sku();
+	}
+
+	public function get_product_name()
+    {
+        return $this->product->get_name();
+    }
+
+    public function get_product_regular_price()
+    {
+        return $this->product->get_regular_price();
+    }
+
+    public function get_product_sale_price()
+    {
+        return $this->product->get_sale_price();
+    }
+
+	public function get_product_children()
+    {
+        if ($this->get_product_type() === 'variable'){
+            return $this->product->get_children();
+        }
+        return false;
+    }
 
 }

a4af55683a9eaf141000f45b94ccb09ba4cb0576
Ebrahim Moeini <ebi3102@gmail.com>
2023-03-02 06:37:49 +0330

    Create raw class of EWPU_Form_Update_Price_By_List to handle emo_ewpu_update_products_price_list

---
+++ includes/Form_Handlers/EWPU_Form_Update_Price_By_List.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace EmoWooPriceUpdate\Form_Handlers;
+
+class EWPU_Form_Update_Price_By_List implements EWPU_Form_Field_Setter, EWPU_Form_Submit {
+
+	use  EWPU_Form_Handler;
+
+	/**
+	 * @inheritDoc
+	 */
+	function field_setter( $fields ): void {
+		// TODO: Implement field_setter() method.
+	}
+
+	public function submit( array $args ): array {
+		// TODO: Implement submit() method.
+		return [];
+	}
+}

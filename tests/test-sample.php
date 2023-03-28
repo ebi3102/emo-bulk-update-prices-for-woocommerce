68833186cf562af06ed026051a9146261fef18b1
Ebrahim Moeini <ebi3102@gmail.com>
2023-03-28 11:38:54 +0430

    Install phpunit test via wp-cli

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Emo_Woo_Price_Update
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}

61cb8fb0b7a5dd90e1940dd6c102ce7792a061b0
Ebrahim Moeini <ebi3102@gmail.com>
2023-04-08 14:09:23 +0330

    Rename emo_ewpu to emo_bupw

--- emo-bulk-update-prices-for-woocommerce.php
+++ emo-bulk-update-prices-for-woocommerce.php
@@ -27,13 +27,13 @@ define('EMO_BUPW_UPLOAD_DIR', $upload_base['basedir'] . '/emo_bupw/uploadedFiles
 define('EMO_BUPW_CREATED_URI', $upload_base['baseurl'] . '/emo_bupw/CreatedFiles/');
 define('EMO_BUPW_UPLOAD_URI', $upload_base['baseurl'] . '/emo_bupw/uploadedFiles/');
 
-if ( ! function_exists( 'emo_ewpu_init' ) ) {
-	add_action( 'plugins_loaded', 'emo_ewpu_init', 11 );
+if ( ! function_exists( 'emo_bupw_init' ) ) {
+	add_action( 'plugins_loaded', 'emo_bupw_init', 11 );
 
-	function emo_ewpu_init() {
+	function emo_bupw_init() {
 
         if ( ! function_exists( 'WC' ) || ! version_compare( WC()->version, '3.0', '>=' ) ) {
-			add_action( 'admin_notices', 'emo_ewpu_notice_wc' );
+			add_action( 'admin_notices', 'emo_bupw_notice_wc' );
 			return;
 		}
 
@@ -54,24 +54,26 @@ if ( ! function_exists( 'emo_ewpu_init' ) ) {
             include_once( "includes/functions/enqueue.php" );
 
             //register scripts
-            add_action('admin_enqueue_scripts', 'emo_ewpu_scripts');
+            add_action('admin_enqueue_scripts', 'emo_bupw_scripts');
         }
         
         /**
          * Load the plugin text domain for translation.
          */
-        add_action( 'init', 'emo_ewpu_load_textdomain' );
+        add_action( 'init', 'emo_bupw_load_textdomain' );
 
-        function emo_ewpu_load_textdomain() {
-            load_plugin_textdomain( 'emo-bulk-update-prices-for-woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-        }
+		if ( ! function_exists( 'emo_bupw_load_textdomain' )){
+            function emo_bupw_load_textdomain() {
+                load_plugin_textdomain( 'emo-bulk-update-prices-for-woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+            }
+		}
     }
 } else {
-	add_action( 'admin_notices', 'emo_ewpu_notice_faulty' );
+	add_action( 'admin_notices', 'emo_bupw_notice_faulty' );
 }
 
-if ( ! function_exists( 'emo_ewpu_notice_wc' ) ) {
-	function emo_ewpu_notice_wc() {
+if ( ! function_exists( 'emo_bupw_notice_wc' ) ) {
+	function emo_bupw_notice_wc() {
 		?>
         <div class="error">
             <p><strong>Emo Woo Update Prices</strong> requires WooCommerce version 3.0 or greater.</p>
@@ -80,8 +82,8 @@ if ( ! function_exists( 'emo_ewpu_notice_wc' ) ) {
 	}
 }
 
-if ( ! function_exists( 'emo_ewpu_notice_faulty' ) ) {
-	function emo_ewpu_notice_faulty() {
+if ( ! function_exists( 'emo_bupw_notice_faulty' ) ) {
+	function emo_bupw_notice_faulty() {
 		?>
         <div class="error">
             <p>Seems there is an error in installation of <strong>Emo Woo Update Prices</strong>. Please
--- includes/functions/enqueue.php
+++ includes/functions/enqueue.php
@@ -13,9 +13,11 @@
  *Load styles and scripts
  *
  */
-function emo_ewpu_scripts($hook){
-    if($hook == 'toplevel_page_emo_ewpu_slug'){
-        wp_register_style('emo_ewpu_admin_style', EMO_BUPW_URI . 'assets/css/style.admin.css', array(), '1.0.0', 'all');
-        wp_enqueue_style('emo_ewpu_admin_style');
-    }
+if ( ! function_exists( 'emo_bupw_scripts' )) {
+	function emo_bupw_scripts( $hook ) {
+		if ( $hook == 'toplevel_page_emo_ewpu_slug' ) {
+			wp_register_style( 'emo_bupw_admin_style', EMO_BUPW_URI . 'assets/css/style.admin.css', array(), '1.0.0', 'all' );
+			wp_enqueue_style( 'emo_bupw_admin_style' );
+		}
+	}
 }

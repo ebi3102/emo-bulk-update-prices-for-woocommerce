726381c020dac576a824896b28a55bb4f1c3953c
Ebrahim Moeini <ebi3102@gmail.com>
2023-04-11 11:02:14 +0330

    Remove return type of methods in EMO_BUPW_Notice_Template.php

--- includes/EMO_BUPW_Notice_Template.php
+++ includes/EMO_BUPW_Notice_Template.php
@@ -9,19 +9,21 @@
 
  namespace EMO_BUPW;
  if (!class_exists('EMO_BUPW_Notice_Template')) {
-	 class EMO_BUPW_Notice_Template {
+	 class EMO_BUPW_Notice_Template
+	 {
 		 private static $template = null;
 		 private static $massage = null;
 		 private static $noticeType = null;
 		 private static $is_dismissible = false;
 
-		 private function __construct() {
-		 }
+		 private function __construct()
+		 {}
 
 		 /**
 		  * @return string|null
 		  */
-		 private static function render_template() {
+		 private static function render_template()
+		 {
 			 if ( self::$is_dismissible ) {
 				 $is_dismissible = 'is-dismissible';
 				 $closeButton    = "<button type='button' class='notice-dismiss'><span class='screen-reader-text'>" .
@@ -46,7 +48,8 @@
 		  *
 		  * @return string|null
 		  */
-		 public static function success( string $massage, bool $is_dismissible = true ) {
+		 public static function success( string $massage, bool $is_dismissible = true )
+		 {
 			 self::$noticeType     = 'success';
 			 self::$is_dismissible = $is_dismissible;
 			 self::$massage        = $massage;
@@ -63,7 +66,8 @@
 		  *
 		  * @return string|null
 		  */
-		 public static function warning( string $massage, bool $is_dismissible = true ) {
+		 public static function warning( string $massage, bool $is_dismissible = true )
+		 {
 			 self::$noticeType     = 'warning';
 			 self::$is_dismissible = $is_dismissible;
 			 self::$massage        = $massage;

30899abd963b988453625968df3050e746b7ffff
Ebrahim Moeini <ebi3102@gmail.com>
2023-04-30 13:15:34 +0330

    Add emo_bupw_after_group_price_form and emo_bupw_before_group_price_form hooks to group price update template

--- includes/templates/group-price-update.php
+++ includes/templates/group-price-update.php
@@ -21,24 +21,29 @@
 <div class="wrap nosubsub">
 
     <div id="col-container" class="wp-clearfix">
-
+        <?php
+        /**
+         * Add content before of form
+         *
+         */
+        do_action('emo_bupw_before_group_price_form');
+        ?>
         <div id="col-left">
             <div class="col-wrap">
                 <div class="form-wrap">
-                    
                     <form method="post">
                         <?php
                         /**
                          * Add element to the group update prices form
-                         *
+                         * add_action('emo_bupw_group_price_form',array(EMO_BUPW_Product_Option_List_Form_Template, 'template'),10);
+                         * add_action('emo_bupw_group_price_form',array(EMO_BUPW_Rate_Type_Form_Template, 'template'),20);
+                         * add_action('emo_bupw_group_price_form',array(EMO_BUPW_Change_Rate_Form_Template, 'template'),30);
+                         * add_action('emo_bupw_group_price_form',array(EMO_BUPW_On_Sale_Activation_Form_Template, 'template'),40);
+                         * add_action('emo_bupw_group_price_form',array(EMO_BUPW_Inc_Dec_Form_Template, 'template'),50);
                          *
                         */
-                        do_action('emo_bupw_group_price_form')
+                        do_action('emo_bupw_group_price_form');
                         ?>
-
-
-
-
                         <div style="padding-top: 20px;">
                             <?php submit_button( esc_html(__('Update', 'emo-bulk-update-prices-for-woocommerce')), esc_attr('primary'), esc_attr('btnSubmit'));  ?>
                         </div>
@@ -47,8 +52,16 @@
                 </div>
             </div>
         </div><!-- #col-left -->
-      
+        <div id="col-right"></div>
+
     </div>
+    <?php
+    /**
+     * Add content before of form
+     *
+     */
+    do_action('emo_bupw_after_group_price_form');
+    ?>
 
    <?php
    echo EMO_BUPW_Notice_Template::success ($successMassage);
